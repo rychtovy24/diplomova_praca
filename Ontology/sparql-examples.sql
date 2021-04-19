94ae8fb448175d72795dac5b1fe12e3409c0ff9d
rychtovy24 <rychtovy@gmail.com>
2021-04-19 08:55:53 +0200

    pridanie konzultanta do prace

--- Ontology/sparql-examples.sql
+++ Ontology/sparql-examples.sql
@@ -46,3 +46,24 @@ WHERE {
 } 
 GROUP BY ?cve ?title
 ORDER BY DESC(?count)
+
+
+PREFIX main: <http://www.semanticweb.org/rycht/ontologies/cyber_security_ontology#>
+PREFIX platform: <http://www.semanticweb.org/rycht/ontologies/cyber_security_ontology/platform#>
+PREFIX cve: <https://cve.mitre.org/about/terminology.html#>
+PREFIX oval:<https://oval.mitre.org/language/version5.11/OVAL>
+
+SELECT ?cve ?title ?product ?family1 ?family2
+WHERE {
+  ?cve a cve:CVE.
+  ?cve main:hasTitle ?title.
+  ?oval1 a oval:.
+  ?oval1 main:hasCVE ?cve.
+  ?oval1 main:affectedProduct ?product.
+  ?oval1 main:affectedFamily ?family1.
+  ?oval2 a oval:.
+  ?oval2 main:hasCVE ?cve.
+  ?oval2 main:affectedProduct ?product.
+  ?oval2 main:affectedFamily ?family2.
+  FILTER (?family1 != ?family2).
+} 
